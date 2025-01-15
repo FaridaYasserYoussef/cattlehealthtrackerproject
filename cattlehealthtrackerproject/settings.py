f4c9c599cf271937f22f8952f53c0a190475a3ca
FaridaYasserYoussef <farida.yasser510@gmail.com>
2025-01-16 00:35:17 +0200

    get db link from env

--- cattlehealthtrackerproject/settings.py
+++ cattlehealthtrackerproject/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = "cattlehealthtrackerproject.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-  'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/cattlehealthtrackerproject',
-        conn_max_age=600
-    )
+  'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+  
+  
+#   dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://postgres:postgres@localhost:5432/cattlehealthtrackerproject',
+#         conn_max_age=600
+#     )
 }
 
 
